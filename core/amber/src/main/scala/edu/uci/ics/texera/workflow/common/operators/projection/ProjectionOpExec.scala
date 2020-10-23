6f6a68dbe904de20ac809684091f97ea659c9048
Bihao Xu <bihaox@uci.edu>
2020-10-23 16:41:34 -0700

    create projection op

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/common/operators/projection/ProjectionOpExec.scala
@@ -0,0 +1,7 @@
+package edu.uci.ics.texera.workflow.common.operators.projection
+
+import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec
+
+class ProjectionOpExec extends MapOpExec{
+
+}
