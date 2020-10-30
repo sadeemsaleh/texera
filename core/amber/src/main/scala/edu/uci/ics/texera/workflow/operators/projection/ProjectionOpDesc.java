0e3f35c8b857c4e8122c228c1801abb059470cea
Bihao Xu <bihaox@uci.edu>
2020-10-30 15:44:59 -0700

    Fix description error

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpDesc.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpDesc.java
@@ -1,4 +1,46 @@
 package edu.uci.ics.texera.workflow.operators.projection;
 
-public class ProjectionOpDesc {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.google.common.base.Preconditions;
+import edu.uci.ics.texera.workflow.common.metadata.OperatorGroupConstants;
+import edu.uci.ics.texera.workflow.common.metadata.OperatorInfo;
+import edu.uci.ics.texera.workflow.common.operators.OneToOneOpExecConfig;
+import edu.uci.ics.texera.workflow.common.operators.map.MapOpDesc;
+import edu.uci.ics.texera.workflow.common.tuple.schema.Schema;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProjectionOpDesc extends MapOpDesc {
+    @JsonProperty(value = "attribute", required = true)
+    @JsonPropertyDescription("a subset of column to keeps")
+    public String attribute;
+
+    @Override
+    public OneToOneOpExecConfig operatorExecutor() {
+        if (attribute == null) {
+            throw new RuntimeException("Projection: attribute is null");
+        }
+        return new OneToOneOpExecConfig(operatorIdentifier(), () -> new ProjectionOpExec(this));
+    }
+
+    @Override
+    public OperatorInfo operatorInfo() {
+        return new OperatorInfo(
+                "Projection",
+                "keeps the column",
+                OperatorGroupConstants.UTILITY_GROUP(),
+                1, 1
+        );
+    }
+
+    @Override
+    public Schema getOutputSchema(Schema[] schemas) {
+        Preconditions.checkArgument(schemas.length == 1);
+        List<String> attributesToRemove = schemas[0].getAttributeNames().stream()
+                .filter(item -> !item.equals(this.attribute))
+                .collect(Collectors.toList());
+        return Schema.newBuilder().add(schemas[0]).removeIfExists(attributesToRemove).build();
+    }
 }
--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.java
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.java
@@ -1,4 +1,31 @@
 package edu.uci.ics.texera.workflow.operators.projection;
 
-public class ProjectionOpExec {
+import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec;
+import edu.uci.ics.texera.workflow.common.tuple.Tuple;
+import scala.Function1;
+import scala.Serializable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class ProjectionOpExec extends MapOpExec {
+    private final ProjectionOpDesc opDesc;
+
+    public ProjectionOpExec(ProjectionOpDesc opDesc) {
+        this.opDesc = opDesc;
+        this.setMapFunc((Function1<Tuple, Tuple> & Serializable) this::processTuple);
+    }
+
+    public Tuple processTuple(Tuple t) {
+        String attribute = opDesc.attribute;
+        System.out.println(attribute);
+        List<String> attributesToRemove = t.getSchema().getAttributeNames().stream()
+                .filter(item -> item.equals(attribute))
+                .collect(Collectors.toList());
+
+        return Tuple.newBuilder().add(t).removeIfExists(attributesToRemove).build();
+    }
+
 }
