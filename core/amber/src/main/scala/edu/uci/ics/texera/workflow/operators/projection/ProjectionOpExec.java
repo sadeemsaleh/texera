ad7e709a7191f935e537675676e549bb457f6b2a
Bihao Xu <bihaox@uci.edu>
2020-10-30 14:53:30 -0700

    Finish projection with one attribute

---
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.java
@@ -0,0 +1,4 @@
+package edu.uci.ics.texera.workflow.operators.projection;
+
+public class ProjectionOpExec {
+}
--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/projection/ProjectionOpExec.scala
+++
@@ -1,7 +0,0 @@
-package edu.uci.ics.texera.workflow.common.operators.projection
-
-import edu.uci.ics.texera.workflow.common.operators.map.MapOpExec
-
-class ProjectionOpExec extends MapOpExec{
-
-}
