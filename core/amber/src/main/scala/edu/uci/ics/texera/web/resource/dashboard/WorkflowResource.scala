7a201e1acc7e7435282770330c8654139f56bc37
Yicong Huang <hyc541978023@gmail.com>
2020-11-29 16:37:38 -0800

    convert to scala

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/WorkflowResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/WorkflowResource.scala
@@ -3,21 +3,22 @@ package edu.uci.ics.texera.web.resource.dashboard
 import java.util
 
 import edu.uci.ics.texera.web.SqlServer
-import edu.uci.ics.texera.web.model.jooq.generated.Tables.{WORKFLOW, WORKFLOW_OF_USER}
-import edu.uci.ics.texera.web.model.jooq.generated.tables.daos.{WorkflowDao, WorkflowOfUserDao}
-import edu.uci.ics.texera.web.model.jooq.generated.tables.pojos.{Workflow, WorkflowOfUser}
 import edu.uci.ics.texera.web.resource.auth.UserResource
+import edu.uci.ics.texera.workflow.jooq.generated.Tables.{WORKFLOW, WORKFLOW_OF_USER}
+import edu.uci.ics.texera.workflow.jooq.generated.tables.daos.{WorkflowDao, WorkflowOfUserDao}
+import edu.uci.ics.texera.workflow.jooq.generated.tables.pojos.{Workflow, WorkflowOfUser}
 import io.dropwizard.jersey.sessions.Session
 import javax.servlet.http.HttpSession
 import javax.ws.rs._
 import javax.ws.rs.core.MediaType
+import org.glassfish.jersey.media.multipart.FormDataParam
 import org.jooq.types.UInteger
 
 /**
-  * This file handles various request related to saved-workflows.
-  * It sends mysql queries to the MysqlDB regarding the UserWorkflow Table
-  * The details of UserWorkflowTable can be found in /core/scripts/sql/texera_ddl.sql
-  */
+ * This file handles various request related to saved-workflows.
+ * It sends mysql queries to the MysqlDB regarding the UserWorkflow Table
+ * The details of UserWorkflowTable can be found in /core/scripts/sql/texera_ddl.sql
+ */
 @Path("/workflow")
 @Produces(Array(MediaType.APPLICATION_JSON))
 class WorkflowResource {
@@ -27,78 +28,106 @@ class WorkflowResource {
   )
 
   /**
-    * This method returns the current in-session user's workflow list
-    *
-    * @param session HttpSession
-    * @return Workflow[]
-    */
+   * This method returns the current in-session user's workflow list
+   *
+   * @param session HttpSession
+   * @return Workflow[]
+   */
   @GET
-  @Path("/get")
-  @Produces(Array(MediaType.APPLICATION_JSON))
-  def retrieveWorkflowsBySessionUser(@Session session: HttpSession): util.List[Workflow] = {
+  @Path("/get") def getUserWorkflow(@Session session: HttpSession): util.List[Workflow] = {
     val user = UserResource.getUser(session)
     if (user == null) return new util.ArrayList[Workflow]() {}
+    getWorkflowsByUser(user.getUserID)
+  }
+
+  /**
+   * select * from table workflow where workflowID is @param "workflowID"
+   *
+   * @param userId an UInteger to identify user
+   * @return Workflow
+   */
+  private def getWorkflowsByUser(userId: UInteger): util.List[Workflow] = {
     SqlServer.createDSLContext
-      .select()
-      .from(WORKFLOW)
-      .join(WORKFLOW_OF_USER)
-      .on(WORKFLOW_OF_USER.WID.eq(WORKFLOW.WID))
-      .where(WORKFLOW_OF_USER.UID.eq(user.getUid))
-      .fetchInto(classOf[Workflow])
+        .select()
+        .from(WORKFLOW)
+        .join(WORKFLOW_OF_USER)
+        .on(WORKFLOW_OF_USER.WID.eq(WORKFLOW.WID))
+        .where(WORKFLOW_OF_USER.UID.eq(userId))
+        .fetchInto(classOf[Workflow])
   }
 
   /**
-    * This method handles the client request to get a specific workflow to be displayed
-    * at current design, it only takes the workflowID and searches within the database for the matching workflow
-    * for future design, it should also take userID as an parameter.
-    *
-    * @param wid     workflow id, which serves as the primary key in the UserWorkflow database
-    * @param session HttpSession
-    * @return a json string representing an savedWorkflow
-    */
+   * This method handles the client request to get a specific workflow to be displayed
+   * at current design, it only takes the workflowID and searches within the database for the matching workflow
+   * for future design, it should also take userID as an parameter.
+   *
+   * @param workflowID workflow id, which serves as the primary key in the UserWorkflow database
+   * @param session    HttpSession
+   * @return a json string representing an savedWorkflow
+   */
   @GET
-  @Path("/get/{wid}")
-  @Produces(Array(MediaType.APPLICATION_JSON))
-  def retrieveWorkflow(@PathParam("wid") wid: UInteger, @Session session: HttpSession): Workflow = {
+  @Path("/get/{workflowID}") def getWorkflow(
+                                                @PathParam("workflowID") workflowID: UInteger,
+                                                @Session session: HttpSession
+                                            ): Workflow = workflowDao.fetchOneByWid(workflowID)
+
+  /**
+   * This method persists the workflow into database
+   *
+   * @param session HttpSession
+   * @param wfId    an UInteger to identify a workflow
+   * @param name    a String, workflow's name
+   * @param content a String, workflow's content
+   * @return Workflow, which contains the generated wfID if not provided
+   */
+  @POST
+  @Path("/save-workflow")
+  @Consumes(Array(MediaType.MULTIPART_FORM_DATA))
+  def saveWorkflow(
+                      @Session session: HttpSession,
+                      @FormDataParam("wfId") wfId: UInteger,
+                      @FormDataParam("name") name: String,
+                      @FormDataParam("content") content: String
+                  ): Workflow = {
     val user = UserResource.getUser(session)
     if (user == null) return null
-    if (
-      workflowOfUserDao.existsById(
-        SqlServer.createDSLContext
-          .newRecord(WORKFLOW_OF_USER.UID, WORKFLOW_OF_USER.WID)
-          .values(user.getUid, wid)
-      )
-    ) {
-      workflowDao.fetchOneByWid(wid)
-    } else {
-      null
-    }
+    if (wfId != null) return updateWorkflow(wfId, name, content)
+    val workflow = insertWorkflowToDataBase(name, content)
+    workflowOfUserDao.insert(new WorkflowOfUser(user.getUserID, workflow.getWid))
+    workflow
   }
 
   /**
-    * This method persists the workflow into database
-    *
-    * @param session  HttpSession
-    * @param workflow , a workflow
-    * @return Workflow, which contains the generated wid if not provided
-    */
-  @POST
-  @Path("/persist")
-  @Consumes(Array(MediaType.APPLICATION_JSON))
-  @Produces(Array(MediaType.APPLICATION_JSON))
-  def persistWorkflow(@Session session: HttpSession, workflow: Workflow): Workflow = {
-    val user = UserResource.getUser(session)
-    if (user == null) return null
-    if (workflow.getWid != null) {
-      // when the wid is provided, update the existing workflow
-      workflowDao.update(workflow)
-      workflow
-    } else {
-      // when the wid is not provided, treat it as a new workflow
-      workflowDao.insert(workflow)
-      workflowOfUserDao.insert(new WorkflowOfUser(user.getUid, workflow.getWid))
-      workflow
-    }
+   * update table workflow set content = @param "content" where wid = @param "workflowId"
+   *
+   * @param workflowId an UInteger to identify workflow
+   * @param name       a String, workflow's name, can be updated
+   * @param content    a String, workflow's content, can be updated
+   * @return Workflow
+   */
+  private def updateWorkflow(workflowId: UInteger, name: String, content: String) = {
+    SqlServer.createDSLContext
+        .update(WORKFLOW)
+        .set(WORKFLOW.NAME, name)
+        .set(WORKFLOW.CONTENT, content)
+        .where(WORKFLOW.WID.eq(workflowId))
+        .execute
+    workflowDao.fetchOneByWid(workflowId)
+  }
 
+  /**
+   * This private method will be used to insert a non existing workflow into the database
+   * There is no request handler that utilize this method yet.
+   *
+   * @param name    a String, workflow's name
+   * @param content a String, workflow's content
+   * @return Workflow
+   */
+  private def insertWorkflowToDataBase(name: String, content: String) = {
+    val workflow = new Workflow
+    workflow.setName(name)
+    workflow.setContent(content)
+    workflowDao.insert(workflow)
+    workflow
   }
 }
