4f0431a1d0ca6b71a73978439c2f4bcf64aab1d4
Yicong Huang <hyc541978023@gmail.com>
2020-12-03 00:22:37 -0800

    fix format

--- core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/WorkflowResource.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/web/resource/dashboard/WorkflowResource.scala
@@ -14,112 +14,113 @@ import javax.ws.rs.core.{MediaType, Response}
 import org.jooq.types.UInteger
 
 /**
- * This file handles various request related to saved-workflows.
- * It sends mysql queries to the MysqlDB regarding the UserWorkflow Table
- * The details of UserWorkflowTable can be found in /core/scripts/sql/texera_ddl.sql
- */
+  * This file handles various request related to saved-workflows.
+  * It sends mysql queries to the MysqlDB regarding the UserWorkflow Table
+  * The details of UserWorkflowTable can be found in /core/scripts/sql/texera_ddl.sql
+  */
 @Path("/workflow")
 @Produces(Array(MediaType.APPLICATION_JSON))
 class WorkflowResource {
-    final private val workflowDao = new WorkflowDao(SqlServer.createDSLContext.configuration)
-    final private val workflowOfUserDao = new WorkflowOfUserDao(
-        SqlServer.createDSLContext.configuration
+  final private val workflowDao = new WorkflowDao(SqlServer.createDSLContext.configuration)
+  final private val workflowOfUserDao = new WorkflowOfUserDao(
+    SqlServer.createDSLContext.configuration
+  )
+
+  /**
+    * This method returns the current in-session user's workflow list
+    *
+    * @param session HttpSession
+    * @return Workflow[]
+    */
+  @GET
+  @Path("/get")
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def retrieveWorkflowsBySessionUser(@Session session: HttpSession): util.List[Workflow] = {
+    val user = UserResource.getUser(session)
+    if (user == null) return new util.ArrayList[Workflow]() {}
+    SqlServer.createDSLContext
+      .select()
+      .from(WORKFLOW)
+      .join(WORKFLOW_OF_USER)
+      .on(WORKFLOW_OF_USER.WID.eq(WORKFLOW.WID))
+      .where(WORKFLOW_OF_USER.UID.eq(user.getUid))
+      .fetchInto(classOf[Workflow])
+  }
+
+  /**
+    * This method handles the client request to get a specific workflow to be displayed
+    * at current design, it only takes the workflowID and searches within the database for the matching workflow
+    * for future design, it should also take userID as an parameter.
+    *
+    * @param wid     workflow id, which serves as the primary key in the UserWorkflow database
+    * @param session HttpSession
+    * @return a json string representing an savedWorkflow
+    */
+  @GET
+  @Path("/get/{wid}")
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def retrieveWorkflow(@PathParam("wid") wid: UInteger, @Session session: HttpSession): Workflow = {
+    val user = UserResource.getUser(session)
+    if (user == null) return null
+    if (workflowOfUserFetchOneByID(wid, user.getUid)) {
+      workflowDao.fetchOneByWid(wid)
+    } else {
+      null
+    }
+  }
+
+  /**
+    * This method persists the workflow into database
+    *
+    * @param session  HttpSession
+    * @param workflow , a workflow
+    * @return Workflow, which contains the generated wid if not provided
+    */
+  @POST
+  @Path("/persist")
+  @Consumes(Array(MediaType.APPLICATION_JSON))
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def persistWorkflow(@Session session: HttpSession, workflow: Workflow): Workflow = {
+    val user = UserResource.getUser(session)
+    if (user == null) return null
+    if (workflowOfUserFetchOneByID(workflow.getWid, user.getUid)) {
+      // when the wid is provided, update the existing workflow
+      workflowDao.update(workflow)
+      workflowDao.fetchOneByWid(workflow.getWid)
+    } else {
+      // when the wid is not provided, treat it as a new workflow
+      workflowDao.insert(workflow)
+      workflowOfUserDao.insert(new WorkflowOfUser(user.getUid, workflow.getWid))
+      workflow
+    }
+
+  }
+
+  /**
+    * This method deletes the workflow from database
+    *
+    * @param session HttpSession
+    * @return Response, deleted - 200, not deleted -
+    */
+  @DELETE
+  @Path("/{wid}")
+  def deleteWorkflow(@PathParam("wid") wid: UInteger, @Session session: HttpSession): Response = {
+    val user = UserResource.getUser(session)
+    if (user == null) return null
+    if (workflowOfUserFetchOneByID(wid, user.getUid)) {
+      workflowDao.deleteById(wid)
+      Response.ok().build()
+    } else {
+      Response.notModified().build()
+    }
+
+  }
+
+  private def workflowOfUserFetchOneByID(wid: UInteger, uid: UInteger): Boolean = {
+    workflowOfUserDao.existsById(
+      SqlServer.createDSLContext
+        .newRecord(WORKFLOW_OF_USER.UID, WORKFLOW_OF_USER.WID)
+        .values(uid, wid)
     )
-
-    /**
-     * This method returns the current in-session user's workflow list
-     *
-     * @param session HttpSession
-     * @return Workflow[]
-     */
-    @GET
-    @Path("/get")
-    @Produces(Array(MediaType.APPLICATION_JSON))
-    def retrieveWorkflowsBySessionUser(@Session session: HttpSession): util.List[Workflow] = {
-        val user = UserResource.getUser(session)
-        if (user == null) return new util.ArrayList[Workflow]() {}
-        SqlServer.createDSLContext
-            .select()
-            .from(WORKFLOW)
-            .join(WORKFLOW_OF_USER)
-            .on(WORKFLOW_OF_USER.WID.eq(WORKFLOW.WID))
-            .where(WORKFLOW_OF_USER.UID.eq(user.getUid))
-            .fetchInto(classOf[Workflow])
-    }
-
-    /**
-     * This method handles the client request to get a specific workflow to be displayed
-     * at current design, it only takes the workflowID and searches within the database for the matching workflow
-     * for future design, it should also take userID as an parameter.
-     *
-     * @param wid     workflow id, which serves as the primary key in the UserWorkflow database
-     * @param session HttpSession
-     * @return a json string representing an savedWorkflow
-     */
-    @GET
-    @Path("/get/{wid}")
-    @Produces(Array(MediaType.APPLICATION_JSON))
-    def retrieveWorkflow(@PathParam("wid") wid: UInteger, @Session session: HttpSession): Workflow = {
-        val user = UserResource.getUser(session)
-        if (user == null) return null
-        if (workflowOfUserFetchOneByID(wid, user.getUid)) {
-            workflowDao.fetchOneByWid(wid)
-        } else {
-            null
-        }
-    }
-
-    /**
-     * This method persists the workflow into database
-     *
-     * @param session  HttpSession
-     * @param workflow , a workflow
-     * @return Workflow, which contains the generated wid if not provided
-     */
-    @POST
-    @Path("/persist")
-    @Consumes(Array(MediaType.APPLICATION_JSON))
-    @Produces(Array(MediaType.APPLICATION_JSON))
-    def persistWorkflow(@Session session: HttpSession, workflow: Workflow): Workflow = {
-        val user = UserResource.getUser(session)
-        if (user == null) return null
-        if (workflowOfUserFetchOneByID(workflow.getWid, user.getUid)) {
-            // when the wid is provided, update the existing workflow
-            workflowDao.update(workflow)
-            workflowDao.fetchOneByWid(workflow.getWid)
-        } else {
-            // when the wid is not provided, treat it as a new workflow
-            workflowDao.insert(workflow)
-            workflowOfUserDao.insert(new WorkflowOfUser(user.getUid, workflow.getWid))
-            workflow
-        }
-
-    }
-
-    /**
-     * This method deletes the workflow from database
-     *
-     * @param session HttpSession
-     * @return Response, deleted - 200, not deleted -
-     */
-    @DELETE
-    @Path("/{wid}")
-    def deleteWorkflow(@PathParam("wid") wid: UInteger, @Session session: HttpSession): Response = {
-        val user = UserResource.getUser(session)
-        if (user == null) return null
-        if (workflowOfUserFetchOneByID(wid, user.getUid)) {
-            workflowDao.deleteById(wid)
-            Response.ok().build()
-        } else {
-            Response.notModified().build()
-        }
-
-    }
-
-    private def workflowOfUserFetchOneByID(wid: UInteger, uid: UInteger): Boolean = {
-        workflowOfUserDao.existsById(
-            SqlServer.createDSLContext
-                .newRecord(WORKFLOW_OF_USER.UID, WORKFLOW_OF_USER.WID)
-                .values(uid, wid))
-    }
+  }
 }
